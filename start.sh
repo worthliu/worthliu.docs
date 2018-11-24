f2bd7333f93c8cb10e259d71a4c90380d1965585
worthliu <1031559976@qq.com>
2018-11-24 22:29:53 +0800

    update startup scripts

--- start.sh
+++ start.sh
@@ -8,13 +8,12 @@ isRun=`ps -ef | grep "docsify" | grep -v grep | wc -l`
 if [ $isRun = "1" ]; then
    echo "docsify is runing....."
    echo "automatically stop docsify service"
-   ps -ef | grep node | grep -v grep | awk '{print $2}' | xargs kill -9
+   ps -ef | grep "docsify" | grep -v grep | awk '{print $2}' | xargs kill -9
    echo "stoping service ends"
 fi
 
 #git pull 
 echo "start docsify service...."
 docsify serve --port 5000 docs
-echo "start docsify service end...."
 
 
